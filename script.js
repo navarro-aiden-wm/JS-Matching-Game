7e857ffde4c5242868349af1772237e206db27a6
Aiden Navarro <aiden.navarro@west-mec.org>
2016-08-12 18:05:38 -0700

    no message

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by session2 on 8/12/16.
+ */
+var numberOfFaces = 5;
+var theLeftSide = document.getElementById("leftSide");
+
+function generateFaces() {
+    var  = document.createElement("FACE");
+    document.body.appendChild(img);
+}
