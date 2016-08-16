1108f279e60c882de1053dab4ea794babbf84138
Aiden Navarro <aiden.navarro@west-mec.org>
2016-08-15 17:54:42 -0700

    no message

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 /**
  * Created by session2 on 8/12/16.
  */
-var numberOfFaces = 5;
-var theLeftSide = document.getElementById("leftSide");
-
 function generateFaces() {
-    var  = document.createElement("FACE");
-    document.body.appendChild(img);
+    var theLeftSide = document.getElementById("leftSide");
+    var numberOfFaces = 5;
+    var theRightSide = document.getElementById("rightSide");
+    var theBody = document.getElementsByTagName("body")[0];
+
+    for (var i = 0; i < numberOfFaces; i++) {
+        var img = document.createElement("img");
+        img.src="Smile.png";
+        img.height = 50;
+        img.width = 50;
+        img.style.top = Math.random() * 400 + "px";
+        img.style.left = Math.random() * 400 + "px";
+        theLeftSide.appendChild(img);
+        var cln = theLeftSide.cloneNode(true);
+        theRightSide.appendChild(cln)
+    }
+
+    theLeftSide.lastChild.onclick=
+        function nextLevel(event){
+            event.stopPropagation();
+            numberOfFaces += 5;
+            generateFaces();
+        };
+
+    theBody.onclick = function gameOver() {
+        alert("Game Over!");
+
+        theBody.onclick = null;
+        theLeftSide.lastChild.onclick = null;
+    };
 }
+
